8f299a53cbef21dcd6ec97a31447b224d78cc862
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-03-27 19:06:39 +0200

    made the code more readable

--- caching-proxy/middlewares/proxy.cs
+++ caching-proxy/middlewares/proxy.cs
@@ -2,24 +2,28 @@ using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 using System.Net.Http;
 
-public class Proxy{
+public class Proxy
+{
     private readonly RequestDelegate _next;
     private readonly HttpClient _client;
     private readonly Cache _cache;
     private readonly proxyConfig _config;
     private const int ttl = 60;
 
-    public Proxy(RequestDelegate next, proxyConfig config, Cache cache){
+    public Proxy(RequestDelegate next, proxyConfig config, Cache cache)
+    {
         _next = next;
         _config = config;
         _client = new HttpClient();
         _cache = cache;
     }
 
-    public async Task Invoke(HttpContext context){
+    public async Task Invoke(HttpContext context)
+    {
         var key = context.Request.Path + context.Request.QueryString;
         var cachedResponse = await _cache.GetCacheResponseAsync(key);
-        if(cachedResponse != null){
+        if(cachedResponse != null)
+        {
             context.Response.Headers["X-Cache"] = "HIT";
             await context.Response.WriteAsync(cachedResponse);
             return;
@@ -29,11 +33,12 @@ public class Proxy{
         var response = await _client.GetAsync(targetUrl);
         var responseString = await response.Content.ReadAsStringAsync();
 
-        if(response.IsSuccessStatusCode){
-        context.Response.Headers["X-Cache"]=  "MISS";
-        await _cache.SetCacheResponseAsync(key, responseString, ttl);
-        await context.Response.WriteAsync(responseString);
-        context.Response.StatusCode = (int)response.StatusCode;
+        if(response.IsSuccessStatusCode)
+        {
+            context.Response.Headers["X-Cache"]=  "MISS";
+            await _cache.SetCacheResponseAsync(key, responseString, ttl);
+            await context.Response.WriteAsync(responseString);
+            context.Response.StatusCode = (int)response.StatusCode;
         } else{
             context.Response.Headers["X-Cache"] = "ERROR";
             context.Response.StatusCode = (int)response.StatusCode;
